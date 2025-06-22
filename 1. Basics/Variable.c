b3c7678532e326cf4b53e2481679c3e99cb91815
Prothasha Deb Nath <prothashadebnath@gmail.com>
2025-06-23 00:35:06 +0600

    Update Variable.c

--- 1. Basics/Variable.c
+++ 1. Basics/Variable.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+int main() {
+    char name = 'P';
+    int age = 22;
+    float pi = 3.14;
+    char percentage = '%';
+
+    printf("My name is %c.\n", name);
+    printf("Age is %d.\n", age);
+    printf("Pi is %f.\n", pi);
+    printf("Percentage is 10%c.", percentage);
+    return 0;
+}
 
