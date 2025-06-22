b3f7b99aba9d57a569e492596374d1d09a5e6ae7
Prothasha Deb Nath <prothashadebnath@gmail.com>
2025-06-23 00:49:10 +0600

    Add files via upload

---
+++ 2. Operators/Increment and Decrement Operator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a = 5;
+    printf("Post-increment: %d\n", a++);
+    printf("After increment: %d\n", a);
+    printf("Pre-decrement: %d\n", --a);
+    return 0;
+}
