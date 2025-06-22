53901b64b7f7f20cdb070f4761991183dc7411bc
Prothasha Deb Nath <prothashadebnath@gmail.com>
2025-06-23 00:51:21 +0600

    Update 1. Basic.c

--- 3. Calculation/1. Basic.c
+++ 3. Calculation/1. Basic.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+int main() {
+    int a, b;
 
+    printf("A is: ");
+    scanf("%d", &a);
+    printf("B is: ");
+    scanf("%d", &b);
+
+    int sum=a+b;
+    int sub=a-b;
+    int multi=a*b;
+    int div=a/b;
+    int mod=a%b;
+
+    printf("Sum is: %d\n", sum);
+    printf("Sub is: %d\n", sub);
+    printf("Multi is: %d\n", multi);
+    printf("Div is: %d\n", div);
+    printf("Mod is: %d", mod);
+
+    return 0;
+}
