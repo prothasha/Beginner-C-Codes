06a73f4ac598d1603b4a1d54afb7799514b94748
Prothasha Deb Nath <prothashadebnath@gmail.com>
2025-06-23 00:58:31 +0600

    Update 1. If (Age).c

--- If-else/1. If (Age).c
+++ If-else/1. If (Age).c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main() {
 
+    int age;
+    printf("Age is: ");
+    scanf("%d", &age);
+    
+    if (age>18) {
+        printf("Adult \n");
+        printf("You can go \n");
+    }
+    else if (age == 18) {
+        printf("You need a pass \n");
+    }
+    else if (age<18) {
+        printf("Child \n");
+    }
+    printf("Thank you 🌸");
+
+    return 0;
+}
