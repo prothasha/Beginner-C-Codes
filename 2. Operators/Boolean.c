28681a468bacf5efd5f4a0b07a9981a1e7ee678c
Prothasha Deb Nath <prothashadebnath@gmail.com>
2025-06-23 00:55:27 +0600

    Add files via upload

---
+++ 2. Operators/Boolean.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main() {
+    int a;
+    printf("A is: ");
+    scanf("%d", &a);
+    bool isGreaterThanTen;
+    if (isGreaterThanTen) {
+        printf("Nice\n");
+    } else {
+        printf("Not nice\n");
+    }
+    }
+
